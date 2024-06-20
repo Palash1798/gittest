9781b57ab4dd1afed414c0453b1e6d2a7bde0ae7
Palashk <kpalash1998@gmail.com>
2024-06-20 17:16:34 +0530

    initial commit

---
+++ src/main/java/com/palash/demotemp/DemotempApplication.java
@@ -0,0 +1,13 @@
+package com.palash.demotemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palash/demotemp/DemotempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palash.demotemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
